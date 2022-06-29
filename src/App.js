aca536fa03f4441bbfb6ab5b5ffff366efebcf87
Sebnem <sebnemgormus@gmail.com>
2022-06-29 21:12:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home"
+import Units from "./pages/Units"
+import UnitDetails from "./pages/UnitDetail"
+import { Route, Switch} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/units">
+            <Units/>
+          </Route>
+          <Route path="/unitdetail">
+            <UnitDetails/>
+          </Route>
+        </Switch>
+    </>
   );
 }
 
